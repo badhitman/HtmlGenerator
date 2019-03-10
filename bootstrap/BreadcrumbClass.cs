94621c1a5eb86a8fcf594ff1204e892a35c80ee4
Станислав Успенский <10000335+badhitman@users.noreply.github.com>
2019-03-10 09:17:43 +0300

    Update BreadcrumbClass.cs

--- bootstrap/BreadcrumbClass.cs
+++ bootstrap/BreadcrumbClass.cs
@@ -28,11 +28,12 @@ namespace HtmlGenerator.bootstrap
                 li my_li;
                 foreach (BreadcrumbItem bi in Breadcrumbs)
                 {
-                    my_li = new li(bi.text) { css_class = "breadcrumb-item" };
+                    my_li = new li(null) { css_class = "breadcrumb-item" };
                     if (string.IsNullOrEmpty(bi.href))
                     {
                         my_li.css_class += " active";
                         my_li.SetAtribute("aria-current", "page");
+                        my_li.InnerText = bi.text;
                     }
                     else
                     {
