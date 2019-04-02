1509ac5504f79208f2fd977b0641d195ac93fd48
badhitman <10000335+badhitman@users.noreply.github.com>
2019-04-02 13:45:54 +0300

    ~

--- DOM/Bootstrap/Forms/Checkboxes/CheckboxInput.cs
+++ DOM/Bootstrap/Forms/Checkboxes/CheckboxInput.cs
@@ -45,7 +45,7 @@ namespace HtmlGenerator.DOM.Bootstrap.Forms
             ////////////////////////////////////////////////
             // Фиксим проблему кроссбраузерности отправок формой чекбокса.
             // Разные браузеры могут по разному отправлять чекбокс. Для стандартизации этой процедуры с чекбоксом в паре держим его "тень" в виде скрытого input-а и контролируем значение теневого значения синхронизируя с основным
-            Childs.Add(new input() { type = InputTypesEnum.hidden, value = (Input.@checked ? "on" : "off"), Name_DOM = Id_DOM });
+            Childs.Add(new input() { type = InputTypesEnum.hidden, value = Input.@checked ? "on" : "off", Name_DOM = Id_DOM });
             //
             script my_script = new script();
             my_script.Childs.Add(new text("jQuery(document).ready(function () {"));
