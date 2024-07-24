d9fa58de492eb7266dc385634e9255aea0fca0ef
stas <ru.usa.goo@gmail.com>
2024-07-24 21:23:53 +0300

    ~

--- MudBlazorProvider/Forms/MudTextFieldProvider.cs
+++ MudBlazorProvider/Forms/MudTextFieldProvider.cs
@@ -10,10 +10,22 @@ namespace HtmlGenerator.mud;
 public class MudTextFieldProvider : MudBaseFieldProvider
 {
     /// <summary>
-    /// InputType
+    /// Property: InputType
     /// </summary>
     public required string InputType { get; set; }
 
+    /// <summary>
+    /// Property: T
+    /// </summary>
+    public required string DescriptorType { get; set; }
+
     /// <inheritdoc/>
     public override string? tag_custom_name => "MudTextField";
+
+    /// <inheritdoc/>   
+    public override string GetHTML(int deep = 0)
+    {
+        SetAttribute("T", $"{DescriptorType}{(IsNullable && !DescriptorType.EndsWith("?") ? "?" : "")}");
+        return base.GetHTML(deep);
+    }
 }
